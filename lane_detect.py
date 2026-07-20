ed5eb300dd40707b4bc31a2ee98d06a0a6f3f033
Ana-Maria <anapetrila2000@gmail.com>
2026-07-20 12:13:11 +0300

    frame resized

--- lane_detect.py
+++ lane_detect.py
@@ -3,26 +3,25 @@ import cv2
 
 cam = cv2.VideoCapture("Lane Detection Test Video 01.mp4")
 
+first_frame = True
+
 while True:
-
     ret, frame = cam.read()
-
-    # ret (bool): Return code of the `read` operation. Did we get an image or not?
-    #             (if not maybe the camera is not detected/connected etc.)
-
-    # frame (array): The actual frame as an array.
-    #                Height x Width x 3 (3 colors, BGR) if color image.
-    #                Height x Width if Grayscale
-    #                Each element is 0-255.
-    #                You can slice it, reassign elements to change pixels, etc.
 
     if ret is False:
         break
 
-    cv2.imshow('Original', frame)
+    if first_frame is True:
+        print("dimensiune originală =", frame.shape)
+        first_frame = False
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    frame = cv2.resize(frame, (480, 270))
+
+    cv2.imshow("Original resized", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
+
 
 cam.release()
 cv2.destroyAllWindows()
