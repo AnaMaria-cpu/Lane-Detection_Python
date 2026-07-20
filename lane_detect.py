2064a7762d2e70de58177514467341c9a042ef3a
Ana-Maria <anapetrila2000@gmail.com>
2026-07-20 17:53:21 +0300

    Converted to grayscale

--- lane_detect.py
+++ lane_detect.py
@@ -1,5 +1,5 @@
 import cv2
-
+import numpy as np
 
 cam = cv2.VideoCapture("Lane Detection Test Video 01.mp4")
 
@@ -14,11 +14,30 @@ while True:
     if first_frame is True:
         print("dimensiune originală =", frame.shape)
         first_frame = False
-
     frame = cv2.resize(frame, (480, 270))
+    height, width, channels = frame.shape
+
+    # TASK 3: creăm manual o imagine grayscale goală
+    gray_frame = np.zeros((height, width), dtype=np.uint8)
+    # Parcurgem fiecare pixel
+    for row in range(height):
+        for column in range(width):
+            blue = frame[row, column, 0]
+            green = frame[row, column, 1]
+            red = frame[row, column, 2]
+
+            # Transformăm cele 3 culori într-o singură valoare grayscale
+            gray_value = int(
+                0.114 * blue +
+                0.587 * green +
+                0.299 * red
+            )
+
+            gray_frame[row, column] = gray_value
+
 
     cv2.imshow("Original resized", frame)
-
+    cv2.imshow("Grayscale manual", gray_frame)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
 
