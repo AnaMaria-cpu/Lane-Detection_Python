d14944accf08ed1310bdeda641baa3c86e37a7bb
Ana-Maria <anapetrila2000@gmail.com>
2026-07-20 18:12:17 +0300

    Selected the road only

--- lane_detect.py
+++ lane_detect.py
@@ -19,14 +19,12 @@ while True:
 
     # TASK 3: creăm manual o imagine grayscale goală
     gray_frame = np.zeros((height, width), dtype=np.uint8)
-    # Parcurgem fiecare pixel
     for row in range(height):
         for column in range(width):
             blue = frame[row, column, 0]
             green = frame[row, column, 1]
             red = frame[row, column, 2]
 
-            # Transformăm cele 3 culori într-o singură valoare grayscale
             gray_value = int(
                 0.114 * blue +
                 0.587 * green +
@@ -35,12 +33,34 @@ while True:
 
             gray_frame[row, column] = gray_value
 
+    # TASK 4: selectăm doar zona drumului
+
+    upper_right = (int(width * 0.52), int(height * 0.76))
+    upper_left = (int(width * 0.45), int(height * 0.76))
+    lower_left = (0, height - 1)
+    lower_right = (width - 1, height - 1)
+
+    trapezoid_points = np.array(
+        [upper_right, upper_left, lower_left, lower_right],
+        dtype=np.int32
+    )
+
+    trapezoid_frame = np.zeros((height, width), dtype=np.uint8)
+
+    cv2.fillConvexPoly(
+        trapezoid_frame,
+        trapezoid_points,
+        1
+    )
+
+    road_frame = gray_frame * trapezoid_frame
 
     cv2.imshow("Original resized", frame)
     cv2.imshow("Grayscale manual", gray_frame)
+    cv2.imshow("Trapezoid", trapezoid_frame * 255)
+    cv2.imshow("Road only", road_frame)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break
-
 
 cam.release()
 cv2.destroyAllWindows()
